5e8ae95c0950cde5d198a682f840e81b5a2f5403
Kabir Gupta <kabirg108@gmail.com>
2023-05-25 10:12:28 -0400

    added more to the genius

--- genius.py
+++ genius.py
@@ -1,3 +1,9 @@
 from pygenius import *
 from api_key import key
+import requests
+
+artist_id = 23796 # twenty one pilots artist id, magic number but shouldn't break
+
+response = requests.get(f"http://api.genius.com/artists/{artist_id}/songs")
+json_data = response.json()
 
